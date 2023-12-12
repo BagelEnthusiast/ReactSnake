216f647fceace28fa9a6ddbaaf465f7037aaa8c5
BagelEnthusiast <nathan.richardson94@gmail.com>
2023-12-12 16:19:42 -0600

    apple now moves when eaten

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,18 @@ import { Point } from './interfaces'
 function App() {
   const [block, setBlock] = useState<Point>({ x: 5, y: 5 });
   const [tail, setTail] = useState<Point>({x: 5, y: 4})
+  const [apple, setApple] = useState<Point>({ x: 10, y: 8 });
   const [direction, setDirection] = useState('Up')
+
+  useEffect(() => {
+    if (block.x === apple.x && block.y === apple.y) {
+      console.log('hitting apple')
+      const newApple = { ...apple };
+      newApple.x = Math.floor(Math.random() * (20 - 1 + 1)) + 1
+      newApple.y = Math.floor(Math.random() * (20 - 1 + 1)) + 1
+      setApple(newApple)
+    }
+  }, [block])
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -36,7 +47,7 @@ function App() {
         setBlock(newBlock)
         setTail({ ...block })
       }
-    }, 1000);
+    }, 500);
   
     return () => clearInterval(interval);
   }, [direction, block])
@@ -66,7 +77,7 @@ function App() {
       {/* todo just for testing */}
       <Block point={tail} color="green" />
       <Block point={{ x: 4, y: 2 }} color="blue" />
-      <Block point={{ x: 10, y: 8 }} color="red" />
+      <Block point={apple} color="red" />
     </>
   )
 }
