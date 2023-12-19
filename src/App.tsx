fda6bac2f5becaa547a6d81e31453817f6ff2963
BagelEnthusiast <nathan.richardson94@gmail.com>
2023-12-19 11:21:52 -0600

    fixed tail logs

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Point } from './interfaces'
 
 function App() {
   const [block, setBlock] = useState<Point>({ x: 5, y: 5 });
-  const [tail, setTail] = useState<Point>({x: 5, y: 4})
+  const [tailArr, setTailArr] = useState<Point[]>([{x: 5, y: 4}])
   const [apple, setApple] = useState<Point>({ x: 10, y: 8 });
   const [direction, setDirection] = useState('Up')
 
@@ -18,6 +18,11 @@ function App() {
       newApple.x = Math.floor(Math.random() * (20 - 1 + 1)) + 1
       newApple.y = Math.floor(Math.random() * (20 - 1 + 1)) + 1
       setApple(newApple)
+      const newTailArr = [ ...tailArr ]
+      const newTailBlock = { ...tailArr[tailArr.length - 1]}
+      
+      newTailArr.push(newTailBlock)
+      setTailArr([ ...newTailArr ])
     }
   }, [block])
 
@@ -26,26 +31,46 @@ function App() {
       if (direction === 'Up') {
         const newBlock = { ...block };
         newBlock.y++;
-        setBlock(newBlock)
-        setTail({ ...block })
+        setBlock(newBlock);
+        const newTailArr = tailArr.map(block => {
+          block.y++
+          return block
+        })
+        console.log('tail:', JSON.stringify(newTailArr))
+        setTailArr([ ...newTailArr ])
       }
       if (direction === 'Down') {
         const newBlock = { ...block };
         newBlock.y--;
-        setBlock(newBlock)
-        setTail({ ...block })
+        setBlock(newBlock);
+        const newTailArr = tailArr.map(block => {
+          block.y++
+          return block
+        })
+        console.log('tail:', JSON.stringify(newTailArr))
+        setTailArr([ ...newTailArr ])
       }
       if (direction === 'Left') {
         const newBlock = { ...block };
         newBlock.x--;
-        setBlock(newBlock)
-        setTail({ ...block })
+        setBlock(newBlock);
+        const newTailArr = tailArr.map(block => {
+          block.y++
+          return block
+        })
+        console.log('tail:', JSON.stringify(newTailArr))
+        setTailArr([ ...newTailArr ])
       }
       if (direction === 'Right') {
         const newBlock = { ...block };
         newBlock.x++;
-        setBlock(newBlock)
-        setTail({ ...block })
+        setBlock(newBlock);
+        const newTailArr = tailArr.map(block => {
+          block.y++
+          return block
+        })
+        console.log('tail:', JSON.stringify(newTailArr))
+        setTailArr([ ...newTailArr ])
       }
     }, 500);
   
@@ -75,7 +100,9 @@ function App() {
       <Block point={block} color="black" />
 
       {/* todo just for testing */}
-      <Block point={tail} color="green" />
+      {tailArr.map(b => {
+        <Block point={b} color="green" />
+      })}
       <Block point={{ x: 4, y: 2 }} color="blue" />
       <Block point={apple} color="red" />
     </>
